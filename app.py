048068cee7ffaf4580e5972c8b52330ad08de0fb
Thiago <thiago.henriqueferreira2909@gmail.com>
2025-04-27 11:35:32 -0400

    Salvando mudanças no app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from EstacionaTech.database.database import verificar_login
 from werkzeug.security import generate_password_hash
+import sys
+from pathlib import Path
 
+# Adiciona o diretório raiz ao Python Path
+sys.path.append(str(Path(__file__).parent))
 
-# Importando os Blueprints
+# Configuração do Flask
+app = Flask(__name__, static_folder='EstacionaTech/static', template_folder='EstacionaTech/templates')
+app.secret_key = 'chave_secreta_super_segura'
+
+# Importação dos Blueprints DEPOIS da criação do app
 from EstacionaTech.routes.auth_routes import auth_bp
 from EstacionaTech.routes.admin_routes import admin_bp
 from EstacionaTech.routes.operador_routes import operador_bp
@@ -14,16 +20,7 @@ from EstacionaTech.routes.setor_routes import setor_bp
 from EstacionaTech.routes.vaga_routes import vaga_bp
 from EstacionaTech.routes.locacao_routes import locacao_bp
 from EstacionaTech.routes.cliente_routes import cliente_bp
-
-app = Flask(__name__, static_folder='EstacionaTech/static', template_folder='EstacionaTech/templates')
-app.secret_key = 'chave_secreta_super_segura'
-
-
-# Definindo rota principal
-@app.route('/')
-def home():
-   return redirect(url_for('auth.login'))
-
+from EstacionaTech.controllers.relatorio_controller import relatorio_bp
 
 # Registrando os blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
@@ -35,6 +32,12 @@ app.register_blueprint(setor_bp, url_prefix='/setor')
 app.register_blueprint(vaga_bp, url_prefix='/vaga')
 app.register_blueprint(cliente_bp, url_prefix='/cliente')
 app.register_blueprint(locacao_bp, url_prefix='/locacao')
+app.register_blueprint(relatorio_bp, url_prefix='/relatorios')
+
+# Definindo rota principal
+@app.route('/')
+def home():
+    return redirect(url_for('auth.login'))
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
